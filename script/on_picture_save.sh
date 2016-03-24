f6eaa2e6d70b7cf45f4b83272be849af1cea027f
David C. Martin <dcmartin@us.ibm.com>
2016-03-24 10:26:33 -0700

    Added call to Alchemy

--- script/on_picture_save.sh
+++ script/on_picture_save.sh
@@ -12,8 +12,12 @@ IMAGE_TYPE=$3
 if [ -n "${VISUAL_USERNAME}" ]; then
     if [ -n "${VISUAL_PASSWORD}" ]; then
         if [ -n "${VISUAL_URL}" ]; then
-	    curl -u "${VISUAL_USERNAME}":"${VISUAL_PASSWORD}" -X POST -F "images_file=@${IMAGE_FILE}" "${VISUAL_URL}" > "${EVENT}.json"
+	    curl -u "${VISUAL_USERNAME}":"${VISUAL_PASSWORD}" -X POST -F "images_file=@${IMAGE_FILE}" "${VISUAL_URL}" > "${EVENT}-visual.json"
         fi
     fi
 fi
-
+if [ -n "${ALCHEMY_API_URL}" ]; then
+    if [ -n "${ALCHEMY_API_KEY}" ]; then
+	curl -X POST --data-binary "@${IMAGE}" "${ALCHEMY_API_URL}?apikey=${ALCHEMY_API_KEY}&imagePostMode=raw&outputMode=json" > "${EVENT}-alchemy.json"
+    fi
+fi
