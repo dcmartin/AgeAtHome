2f6907fcd1ced9bc7e90e894094285a146c65a12
David C. Martin <dcmartin@us.ibm.com>
2016-03-25 12:42:22 -0700

    Setup to evaluate both full and crop image w/ both algorithms

--- script/on_picture_save.sh
+++ script/on_picture_save.sh
@@ -17,9 +17,8 @@ if [ -n "${MOTION_MIDX}" ]; then
 	    if [ -n "${MOTION_HEIGHT}" ]; then
                 (( MOTION_X = ${MOTION_MIDX} - ${MOTION_WIDTH} / 2 ))
                 (( MOTION_Y = ${MOTION_MIDY} - ${MOTION_HEIGHT} / 2 ))
-                convert -crop "${MOTION_WIDTH}x${MOTION_HEIGHT}+${MOTION_X}+${MOTION_Y}" "${IMAGE_FILE}" "${IMAGE_FILE%.*}-crop.jpg"
-		mv "${IMAGE_FILE}" "${IMAGE_FILE%.*}=${MOTION_WIDTH}x${MOTION_HEIGHT}+${MOTION_X}+${MOTION_Y}".jpg
-		mv "${IMAGE_FILE%.*}-crop.jpg" "${IMAGE_FILE}"
+		IMAGE_CROP = "${IMAGE_FILE%.*}=${MOTION_WIDTH}x${MOTION_HEIGHT}+${MOTION_X}+${MOTION_Y}".jpg
+                convert -crop "${MOTION_WIDTH}x${MOTION_HEIGHT}+${MOTION_X}+${MOTION_Y}" "${IMAGE_FILE}" "${IMAGE_CROP}"
 	    fi
 	fi
     fi
@@ -28,14 +27,18 @@ fi
 if [ -n "${VISUAL_USERNAME}" ]; then
     if [ -n "${VISUAL_PASSWORD}" ]; then
         if [ -n "${VISUAL_URL}" ]; then
-	    echo "${VISUAL_USERNAME}":"${VISUAL_PASSWORD}" -X POST -F "images_file=@${IMAGE_FILE}" "${VISUAL_URL}"
-	    curl -q -u "${VISUAL_USERNAME}":"${VISUAL_PASSWORD}" -X POST -F "images_file=@${IMAGE_FILE}" "${VISUAL_URL}" > "${EVENT}-visual.json"
+	    if [ -n "${IMAGE_CROP}" ]; then
+		curl -q -u "${VISUAL_USERNAME}":"${VISUAL_PASSWORD}" -X POST -F "images_file=@${IMAGE_CROP}" "${VISUAL_URL}" > "${EVENT}-crop-visual.json"
+	    fi
+	    curl -q -u "${VISUAL_USERNAME}":"${VISUAL_PASSWORD}" -X POST -F "images_file=@${IMAGE_FILE}" "${VISUAL_URL}" > "${EVENT}-full-visual.json"
         fi
     fi
 fi
 if [ -n "${ALCHEMY_API_URL}" ]; then
     if [ -n "${ALCHEMY_API_KEY}" ]; then
-	echo  "@${IMAGE_FILE}" "${ALCHEMY_API_URL}?apikey=${ALCHEMY_API_KEY}&imagePostMode=raw&outputMode=json"
-	curl -q -X POST --data-binary "@${IMAGE_FILE}" "${ALCHEMY_API_URL}?apikey=${ALCHEMY_API_KEY}&imagePostMode=raw&outputMode=json" > "${EVENT}-alchemy.json"
+        if [ -n "${IMAGE_CROP}" ]; then
+	    curl -q -X POST --data-binary "@${IMAGE_CROP}" "${ALCHEMY_API_URL}?apikey=${ALCHEMY_API_KEY}&imagePostMode=raw&outputMode=json" > "${EVENT}-crop-alchemy.json"
+	fi
+	curl -q -X POST --data-binary "@${IMAGE_FILE}" "${ALCHEMY_API_URL}?apikey=${ALCHEMY_API_KEY}&imagePostMode=raw&outputMode=json" > "${EVENT}-full-alchemy.json"
     fi
 fi
