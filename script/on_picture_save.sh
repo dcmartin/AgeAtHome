b750bbcfcbd6bc85c301857d95edfeb6ac503105
David C. Martin <dcmartin@us.ibm.com>
2016-03-29 10:17:19 -0700

    Fixed variety of issues w/ submitting to CLOUDANT

--- script/on_picture_save.sh
+++ script/on_picture_save.sh
@@ -10,16 +10,17 @@ MOTION_MIDY=$5
 MOTION_WIDTH=$6
 MOTION_HEIGHT=$7
 
-# proceed if CROP_OFF is zero length or undefined
-if [ -z "${CROP_OFF}" ]; then
-    if [ -n "${MOTION_MIDX}" ]; then
-	if [ -n "${MOTION_MIDY}" ]; then
-	    if [ -n "${MOTION_WIDTH}" ]; then
-		if [ -n "${MOTION_HEIGHT}" ]; then
-		    # calculate X,Y start from mid-point and extant
-		    (( MOTION_X = ${MOTION_MIDX} - ${MOTION_WIDTH} / 2 ))
-		    (( MOTION_Y = ${MOTION_MIDY} - ${MOTION_HEIGHT} / 2 ))
-		    IMAGE_CROP="${IMAGE_FILE%.*}:${MOTION_WIDTH}x${MOTION_HEIGHT}+${MOTION_X}+${MOTION_Y}".jpg
+if [ -n "${MOTION_MIDX}" ]; then
+    if [ -n "${MOTION_MIDY}" ]; then
+	if [ -n "${MOTION_WIDTH}" ]; then
+	    if [ -n "${MOTION_HEIGHT}" ]; then
+		# calculate X,Y start from mid-point and extant
+		(( MOTION_X = ${MOTION_MIDX} - ${MOTION_WIDTH} / 2 ))
+		(( MOTION_Y = ${MOTION_MIDY} - ${MOTION_HEIGHT} / 2 ))
+		IMAGE_BOX="${MOTION_WIDTH}x${MOTION_HEIGHT}+${MOTION_X}+${MOTION_Y}"
+		# proceed if CROP_OFF is zero length or undefined
+		if [ -z "${CROP_OFF}" ]; then
+		    IMAGE_CROP="${IMAGE_FILE%.*}:${IMAGE_BOX}".jpg
 		    convert -crop "${MOTION_WIDTH}x${MOTION_HEIGHT}+${MOTION_X}+${MOTION_Y}" "${IMAGE_FILE}" "${IMAGE_CROP}"
 		fi
 	    fi
@@ -27,20 +28,48 @@ if [ -z "${CROP_OFF}" ]; then
     fi
 fi
 
+# check for CLOUDANT db
+if [ -z ${CLOUDANT_OFF} ] && -n "${CLOUDANT_URL}" ] && [ -n ${DEVICE_NAME} ]; then
+    DEVICE_DB=`curl -q -X GET "${CLOUDANT_URL}/_all_dbs" | egrep "${DEVICE_NAME}"`
+    if [ -z "${DEVICE_DB}" ]; then
+        # create DB
+	DEVICE_DB=`curl -q -X PUT "${CLOUDANT_URL}/${DEVICE_NAME}" | egrep "ok"`
+	if [ -z "${DEVICE_DB} ]; then
+	    # DB create failed
+	    echo "DB CREATE FAILURE ${DEVICE_NAME}" >&2
+	    # force off 
+	    CLOUDANT_OFF=TRUE
+	else
+        fi
+    fi
+    if [ -z "${CLOUDANT_OFF}" ]; then
+	DEVICE_DB=`curl -q -X GET "${CLOUDANT_URL}/${DEVICE_NAME}"
+    fi
+else
+    # off or failure
+    CLOUDANT_OFF=TRUE
+fi
+
 # proceed if VISUAL_OFF is zero length or undefined
 if [ -z "${VISUAL_OFF}" ]; then
     if [ -n "${VISUAL_USERNAME}" ]; then
 	if [ -n "${VISUAL_PASSWORD}" ]; then
 	    if [ -n "${VISUAL_URL}" ]; then
-		if [ -n "${IMAGE_CROP}" ]; then
+		if [ -z "${CROP_OFF}" ]; then
+		    # cropped image
 		    curl -q -u "${VISUAL_USERNAME}":"${VISUAL_PASSWORD}" -X POST -F "images_file=@${IMAGE_CROP}" "${VISUAL_URL}" > "${IMAGE_CROP%.*}-visual.json"
 		    if [ -n "${CLOUDANT_URL}" ] && [ -n ${DEVICE_NAME} ]; then
-			curl -q -H "Content-type: application/json" -X POST "$CLOUDANT_URL/${DEVICE_NAME}/${IMAGE_CROP##*/}-visual" -d "@${IMAGE_CROP%.*}-alchemy.json"
+			IMAGE_ID=`echo "${IMAGE_CROP##*/}"`
+			IMAGE_ID=`echo "${IMAGE_ID%.*}-visual"`
+			curl -q -H "Content-type: application/json" -X POST "$CLOUDANT_URL/${DEVICE_NAME}/${IMAGE_ID}-visual" -d "@${IMAGE_CROP%.*}-alchemy.json"
 		    fi
 		fi
+		# full image
 		curl -q -u "${VISUAL_USERNAME}":"${VISUAL_PASSWORD}" -X POST -F "images_file=@${IMAGE_FILE}" "${VISUAL_URL}" > "${IMAGE_FILE%.*}-visual.json"
 		if [ -n "${CLOUDANT_URL}" ] && [ -n ${DEVICE_NAME} ]; then
-		    curl -q -H "Content-type: application/json" -X POST "$CLOUDANT_URL/${DEVICE_NAME}/${IMAGE_FILE##*/}-visual" -d "@${IMAGE_FILE%.*}-alchemy.json"
+		    IMAGE_ID=`echo "${IMAGE_FILE##*/}"`
+		    IMAGE_ID=`echo "${IMAGE_ID%.*}-visual"`
+		    curl -q -H "Content-type: application/json" -X POST "$CLOUDANT_URL/${DEVICE_NAME}/${IMAGE_ID}" -d "@${IMAGE_FILE%.*}-visual.json"
 		fi
 	    fi
 	fi
@@ -58,15 +87,19 @@ if [ -z "${ALCHEMY_OFF}" ]; then
 	    ALCHEMY_API_KEY=${ALCHEMY_API_KEY_PM}
 	fi
 	if [ -n "${ALCHEMY_API_KEY}" ]; then
-	    if [ -n "${IMAGE_CROP}" ]; then
+	    if [ -z "${CROP_OFF}" ]; then
 		curl -q -X POST --data-binary "@${IMAGE_CROP}" "${ALCHEMY_API_URL}?apikey=${ALCHEMY_API_KEY}&imagePostMode=raw&outputMode=json" > "${IMAGE_CROP%.*}-alchemy.json"
 		if [ -n "${CLOUDANT_URL}" ] && [ -n ${DEVICE_NAME} ]; then
-		    curl -q -H "Content-type: application/json" -X POST "$CLOUDANT_URL/${DEVICE_NAME}/${IMAGE_CROP##*/}-alchemy" -d "@${IMAGE_CROP%.*}-alchemy.json"
+		    IMAGE_ID=`echo "${IMAGE_CROP##*/}"`
+		    IMAGE_ID=`echo "${IMAGE_ID%.*}-alchemy"`
+		    curl -q -H "Content-type: application/json" -X POST "$CLOUDANT_URL/${DEVICE_NAME}/${IMAGE_ID}" -d "@${IMAGE_CROP%.*}-alchemy.json"
 		fi
 	    fi
 	    curl -q -X POST --data-binary "@${IMAGE_FILE}" "${ALCHEMY_API_URL}?apikey=${ALCHEMY_API_KEY}&imagePostMode=raw&outputMode=json" > "${IMAGE_FILE%.*}-alchemy.json"
 	    if [ -n "${CLOUDANT_URL}" ] && [ -n ${DEVICE_NAME} ]; then
-		curl -q -H "Content-type: application/json" -X POST "$CLOUDANT_URL/${DEVICE_NAME}/${IMAGE_FILE##*/}-alchemy" -d "@${IMAGE_FILE%.*}-alchemy.json"
+		IMAGE_ID=`echo "${IMAGE_FILE##*/}"`
+		IMAGE_ID=`echo "${IMAGE_ID%.*}-alchemy"`
+		curl -q -H "Content-type: application/json" -X POST "$CLOUDANT_URL/${DEVICE_NAME}/${IMAGE_ID}" -d "@${IMAGE_FILE%.*}-alchemy.json"
 	    fi
 	fi
     fi
