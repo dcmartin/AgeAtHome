6be30dfa57fdf8f2c5a3c1c59b78c4a7c73362f3
David C. Martin <dcmartin@us.ibm.com>
2016-07-25 15:39:53 -0700

    Hopefully now works whether VI or A are operational

--- script/on_picture_save.sh
+++ script/on_picture_save.sh
@@ -5,6 +5,8 @@ echo "+++ BEGIN: $0: $*" $(date) >&2
 EVENT=$1
 IMAGE_FILE=$2
 IMAGE_TYPE=$3
+
+# ignore motion
 MOTION_MIDX=$4
 MOTION_MIDY=$5
 MOTION_WIDTH=$6
@@ -24,8 +26,12 @@ if [ -n "${MOTION_MIDX}" ]; then
     fi
 fi
 
-# proceed if VISUAL_OFF is zero length or undefined
+if [ -z "${IMAGE_BOX}" ]; then
+    IMAGE_BOX="640x480+0+0"
+fi
+
 VISUAL_OUTPUT=""
+# proceed if VISUAL_OFF is zero length or undefined
 if [ -z "${VISUAL_OFF}" ] && [ -n "${VISUAL_USERNAME}" ] && [ -n "${VISUAL_PASSWORD}" ] && [ -n "${VISUAL_URL}" ]; then
 
     # full image
@@ -36,67 +42,81 @@ if [ -z "${VISUAL_OFF}" ] && [ -n "${VISUAL_USERNAME}" ] && [ -n "${VISUAL_PASSW
     # VisualInsights
     curl -q -s -L -u "${VISUAL_USERNAME}":"${VISUAL_PASSWORD}" -X POST -F "images_file=@${IMAGE_FILE}" "${VISUAL_URL}" > "${OUTPUT}"
     if [ -s "${OUTPUT}" ]; then
+	# echo -n "+++ VISUAL_INSIGHTS = "; jq -c '.' "${OUTPUT}"
 	VISUAL_OUTPUT="${OUTPUT}"
-	# DEBUG
-	cat "${OUTPUT}"
     else
 	echo "+++ $0 FAILURE VISUAL_INSIGHTS ${IMAGE_FILE}"
     fi
+else
+    echo "+++ $0 NO VISUAL_INSIGHTS"
 fi
 
 #
 # ALCHEMY
 #
+ALCHEMY_OUTPUT=""
+# echo "+++ ALCHEMY_OFF " [ -z "${ALCHEMY_OFF}" "ALCHEMY_API_KEY = " [ -n "${ALCHEMY_API_KEY}" ] "ALCHEMY_API_URL = " [ -n "${ALCHEMY_API_URL}" ]
+# test if not OFF or not configured
+if [ -z "${ALCHEMY_OFF}" ] && [ -n "${ALCHEMY_API_KEY}" ] && [ -n "${ALCHEMY_API_URL}" ]; then
 
-# use two API keys (morning and evening)
-AMPM=`date +%p`
-if [ ${AMPM} == AM ] && [ -n ${ALCHEMY_API_KEY_AM} ]; then
-    ALCHEMY_API_KEY=${ALCHEMY_API_KEY_AM}
-fi
-if [ ${AMPM} == PM ] && [ -n ${ALCHEMY_API_KEY_PM} ]; then
-    ALCHEMY_API_KEY=${ALCHEMY_API_KEY_PM}
-fi
-# test if ON and unconfigured -- ALCHEMY IS ALWAYS OFF NOW 2016-07-20
-if [ -z "${ALCHEMY_ON}" ] && [ -n "${ALCHEMY_API_KEY}" ] && [ -n "${ALCHEMY_API_URL}" ]; then
     # full  image
     OUTPUT="${IMAGE_FILE%.*}-alchemy.json"
     
     echo "+++ $0 PROCESSING ALCHEMY ${IMAGE_FILE}"
 
     # Alchemy
-    curl -q -s -L -X POST --data-binary "@${IMAGE_FILE}" "${ALCHEMY_API_URL}?apikey=${ALCHEMY_API_KEY}&imagePostMode=raw&outputMode=json" > "${OUTPUT}"
+    # curl -q -s -L -X POST --data-binary "@${IMAGE_FILE}" "${ALCHEMY_API_URL}/image/ImageGetRankedImageFaceTags?apikey=${ALCHEMY_API_KEY}&imagePostMode=raw&outputMode=json" > "${OUTPUT}"
+    curl -q -s -L -X POST --data-binary "@${IMAGE_FILE}" "${ALCHEMY_API_URL}/image/ImageGetRankedImageKeywords?apikey=${ALCHEMY_API_KEY}&imagePostMode=raw&outputMode=json" > "${OUTPUT}"
     if [ -s "${OUTPUT}" ]; then
+	# echo -n "+++ ALCHEMY == "; jq -c '.' "${OUTPUT}"
 	ALCHEMY_OUTPUT="${OUTPUT}"
     else
 	echo "+++ $0 FAILURE ALCHEMY ${IMAGE_FILE}"
     fi
-fi
-
-#
-# CLOUDANT
-#
-
-# Prepare output
-OUTPUT="${IMAGE_FILE%.*}.json"
-if [ -s "${VISUAL_OUTPUT}" ]; then
-    # ALCHEMY is no more
-    echo '"alchemy":{"text":"NO_TAGS","score":""}' > "${OUTPUT}.$$"
-    # process VI results
-    jq -c '.images[0]' "${VISUAL_OUTPUT}" | sed 's/^{\(.*\)/"visual":{ \1 \}/' >> "${OUTPUT}.$$"
-    jq -c '.' "${OUTPUT}.$$" > "${OUTPUT}"
-    # remove tmp & originals
-    rm "${OUTPUT}.$$" "${VISUAL_OUTPUT}" "${ALCHEMY_OUTPUT}"
 else
-    echo "*** ERROR: $0 - NO OUTPUT"
-    exit
+    echo "+++ $0 NO ALCHEMY"
 fi
 
-if [ -s "${OUTPUT}" ]; then
-    # drop prefix path
-    IMAGE_ID=`echo "${OUTPUT##*/}"`
-    # drop extension
-    IMAGE_ID=`echo "${IMAGE_ID%.*}"`
+#
+# Prepare output
+#
+OUTPUT="${IMAGE_FILE%.*}.json"
+# drop prefix path
+IMAGE_ID=`echo "${OUTPUT##*/}"`
+# drop extension
+IMAGE_ID=`echo "${IMAGE_ID%.*}"`
 
+if [ -z "${VISUAL_OUTPUT}" ] && [ -z "${ALCHEMY_OUTPUT}" ]; then
+    echo "+++ $0 NEITHER"
+    echo '{ "alchemy":{"text":"NO_TAGS","score":0},' > "${OUTPUT}.$$"
+    echo '"visual":{"image":"'${IMAGE_ID}.jpg'","scores":[{"classifier_id":"NA","name":"NA","score":0}]' >> "${OUTPUT}.$$"
+elif [ -z "${ALCHEMY_OUTPUT}" ]; then
+    echo "+++ $0 VISUAL_INSIGHTS ONLY"
+    echo '{ "alchemy":{"text":"NO_TAGS","score":0},' > "${OUTPUT}.$$"
+    jq -c '.images[1]' "${VISUAL_OUTPUT}" | sed 's/^{\(.*\)/"visual":{ \1 \}/' >> "${OUTPUT}.$$"
+elif [ -z "${VISUAL_OUTPUT}" ]; then
+    echo "+++ $0 ALCHEMY ONLY"
+    jq -c '.imageKeywords[0]' "${ALCHEMY_OUTPUT}" | sed 's/\(.*\)\}/\{ "alchemy": \1 \},/' > "${OUTPUT}.$$"
+    echo '"visual":{"image":"'${IMAGE_ID}.jpg'","scores":[{"classifier_id":"NA","name":"NA","score":0}]' >> "${OUTPUT}.$$"
+else
+    echo "+++ $0 BOTH"
+    # order matters
+    jq -c '.imageKeywords[0]' "${ALCHEMY_OUTPUT}" | sed 's/\(.*\)\}/\{ "alchemy": \1 \},/' > "${OUTPUT}.$$"
+    jq -c '.images[0]' "${VISUAL_OUTPUT}" | sed 's/^{\(.*\)/"visual":{ \1 \}/' >> "${OUTPUT}.$$"
+fi
+
+echo "}}" >> "${OUTPUT}.$$"
+
+# create (and validate) output
+jq -c '.' "${OUTPUT}.$$" > "${OUTPUT}"
+
+# remove tmp & originals
+rm -f "${OUTPUT}.$$" "${VISUAL_OUTPUT}" "${ALCHEMY_OUTPUT}"
+
+#
+# add date and time
+#
+if [ -s "${OUTPUT}" ]; then
     # add datetime and bounding box information
     if [ -n "${IMAGE_ID}" ] && [ -n "${IMAGE_BOX}" ]; then
 	DATE_TIME=`echo "${IMAGE_ID}" | sed "s/\(.*\)-.*-.*/\1/"`
@@ -125,7 +145,10 @@ else
     exit
 fi
 
-# Cloudant
+#
+# CLOUDANT
+#
+
 if [ -z "${CLOUDANT_OFF}" ] && [ -s "${OUTPUT}" ] && [ -n "${CLOUDANT_URL}" ] && [ -n "${DEVICE_NAME}" ]; then
     DEVICE_DB=`curl -q -s -X GET "${CLOUDANT_URL}/${DEVICE_NAME}" | jq '.db_name'`
     if [ "${DEVICE_DB}" == "null" ]; then
